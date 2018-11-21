094f1a0535f611b19ce0e4da554d5216954903ce
Matthew Khadzhiev <khad-mat123@yandex.ru>
2018-11-21 21:11:06 +0700

    Added currencies constants

---
+++ app/src/main/java/ru/golden/currencyconverter/feature/converter/domain/constants/CurrencyCodeConstants.kt
@@ -0,0 +1,19 @@
+package ru.golden.currencyconverter.feature.converter.domain.constants
+
+/**
+ * User: Matvei Khadzhiev
+ * Date: 21.11.2018
+ * Time: 21:02
+ */
+const val AUSTRALIAN_CODE = "AUD"
+const val BRAZILIAN_CODE = "BRL"
+const val CANADIAN_CODE = "CAD"
+const val CHINESE_CODE = "CNY"
+const val CZECH_CODE = "CZK"
+const val DANISH_CODE = "DKK"
+const val STERLING_CODE = "GBP"
+const val MEXICAN_CODE = "MXN"
+const val NORWEGIAN_CODE = "NOK"
+const val RUSSIAN_CODE = "RUB"
+const val TURKISH_CODE = "TRY"
+const val AMERICAN_CODE = "USD"
---
+++ app/src/main/java/ru/golden/currencyconverter/feature/converter/domain/constants/CurrencyNameConstants.kt
@@ -0,0 +1,19 @@
+package ru.golden.currencyconverter.feature.converter.domain.constants
+
+/**
+ * User: Matvei Khadzhiev
+ * Date: 21.11.2018
+ * Time: 21:02
+ */
+const val AUSTRALIAN_NAME = "Australian dollar"
+const val BRAZILIAN_NAME = "Brazilian real"
+const val CANADIAN_NAME = "Canadian dollar"
+const val CHINESE_NAME = "Yuan"
+const val CZECH_NAME = "Czech koruna"
+const val DANISH_NAME = "Danish krone"
+const val STERLING_NAME = "Pound sterling"
+const val MEXICAN_NAME = "Mexican peso"
+const val NORWEGIAN_NAME = "Norwegian krone"
+const val RUSSIAN_NAME = "Russian ruble"
+const val TURKISH_NAME = "Turkish lira"
+const val AMERICAN_NAME = "United States dollar"
